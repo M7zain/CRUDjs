7c7b8844459820a8293a0947cc3f6aa42d9532d0
M7zain <zeynkara91@gmail.com>
2024-04-30 17:07:44 +0300

    adding the editing functionality to the website

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById("submit").addEventListener("click", (event) => {
             <td>${lastName}</td>
             <td>${phoneNumber}</td>
             <td>
-                <button id="edit-button">Edit</button>
+                <button id="edit-button" onclick ="editRow(this)">Edit</button>
                 <button id="delete-button" onclick="deleteRow(this)">Delete</button>
             </td>
         `;
@@ -32,7 +32,36 @@ document.getElementById("submit").addEventListener("click", (event) => {
 }); 
 
 
+function editRow(button){ 
 
+    let row = button.parentNode.parentNode;
+    let cells = row.getElementsByTagName("td"); 
+
+    let firstName = cells[0].textContent;
+    let lastName  = cells[1].textContent; 
+    let phoneNumber = cells[2].textContent; 
+
+    document.getElementById("addtext").textContent = `you are editing ${firstName}!` ;
+    
+
+    let editedRow = document.getElementsByTagName("tr");
+
+    editedRow.innerHTML = `
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${phoneNumber}</td>
+    <td>
+        <button id="edit-button" onclick ="editRow(this)" >Edit</button>
+        <button id="delete-button" onclick="deleteRow(this)">Delete</button>
+    </td>
+`;
+
+    row.parentNode.insertBefore(editedRow, row); 
+    row.parentNode.removeChild(row); 
+
+
+
+}
 
 
 
